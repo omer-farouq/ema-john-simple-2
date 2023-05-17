38b61ad560c78fe25d004bf7b1301970ad897ac5
unknown <omerfarouqstd@gmail.com>
2023-05-18 05:54:00 +0600

    make a header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -5,7 +5,13 @@ import logo from "../../images/Logo.svg";
 const Header = () => {
     return (
         <nav className='header'>
-            <img src={logo} alt="" srcset="" />
+            <img src={logo} alt="" />
+            <div>
+                <a href="/shop">Shop</a>
+                <a href="/order">Order</a>
+                <a href="/inventory">Inventory</a>
+                <a href="/login">Login</a>
+            </div>
            
         </nav>
     );
