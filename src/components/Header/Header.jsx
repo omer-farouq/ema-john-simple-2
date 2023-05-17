4e1ef7b1352940dc9654115876e2907fbb90b78d
unknown <omerfarouqstd@gmail.com>
2023-05-17 09:19:58 +0600

    header logo comming

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,10 +1,13 @@
 import React from 'react';
+import "./Header.css";
+import logo from "../../images/Logo.svg";
 
 const Header = () => {
     return (
-        <div>
-            <h2>This is a Header!!</h2>
-        </div>
+        <nav className='header'>
+            <img src={logo} alt="" srcset="" />
+           
+        </nav>
     );
 };
 
