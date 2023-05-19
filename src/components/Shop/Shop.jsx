872301c8ba2ee0ac3afdc14edbc70ecad9ba7985
unknown <omerfarouqstd@gmail.com>
2023-05-19 11:46:19 +0600

    added some

--- src/components/Shop/Shop.jsx
+++ src/components/Shop/Shop.jsx
@@ -31,7 +31,7 @@ const Shop = () => {
                 // step 4: add the addedProduct to the savedCart
                 savedCart.push(addedProduct);
             }
-            console.log(addedProduct);
+            // console.log(addedProduct);
         }
         // step 5: set the cart
         setCart(savedCart)
@@ -39,7 +39,20 @@ const Shop = () => {
 
     const handleAddToCart = (product) => {
         // cart.push(product);
-        const newCart = [...cart, product];
+        let newCart = [];
+        // const newCart = [...cart, product];
+        // if product doesn't exist in the cart, then set quantity = 1
+        // if exist update quantity by 1
+        const exists = cart.find(pd => pd.id === product.id);
+        if(!exists){
+            product.quantity = 1;
+            newCart = [...cart, product];
+        }
+        else{
+            exists.quantity = exists.quantity + 1;
+            const remaining = cart.filter(pd => pd.id !== product.id);
+            newCart = [...remaining, exists];
+        }
         setCart(newCart);
         addToDb(product.id);
     };
