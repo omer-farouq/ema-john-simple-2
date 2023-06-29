b64a66be17f6d7db971d560ef492f482e67a3714
unknown <omerfarouqstd@gmail.com>
2023-06-29 20:31:47 +0600

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -2,16 +2,15 @@
 // import reactLogo from './assets/react.svg'
 // import viteLogo from '/vite.svg'
 import './App.css'
-import Header from './components/Header/Header'
-import Shop from './components/Shop/Shop'
+import Contact from './components/Contact/Contact'
+
 
 function App() {
   // const [count, setCount] = useState(0)
 
   return (
     <div className='App'>
-      <Header></Header>
-      <Shop></Shop>
+      <Contact></Contact>
     </div>
   )
 }
