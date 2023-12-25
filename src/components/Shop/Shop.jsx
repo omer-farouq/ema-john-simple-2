543b7236f99c3c78600eec6643d4001f8ee5f788
omer-farouq <omerfarouqstd@gmail.com>
2023-12-25 20:19:46 +0600

    show data from cart

--- src/components/Shop/Shop.jsx
+++ src/components/Shop/Shop.jsx
@@ -27,14 +27,29 @@ const Shop = () => {
                 addedProduct.quantity = quantity;
                 // step 4: add the added product to the saved cart
                 savedCart.push(addedProduct);
-            }
+            } 
             console.log('added Product',addedProduct)
         }
         setCart(savedCart);
     }, [products])
 
     const handleAddtoCart = (product) =>{
-        const newCart = [...cart, product];
+        // cart.push(product);
+        let newCart = [];
+        // const newCart = [...cart, product];
+        // if product exist in the cart, then set quantity = 1
+        // if exist update quantity by 1
+        const exists = cart.find(pd => pd.id === product.id);
+        if(!exists){
+            product.quantity = 1;
+            newCart = [...cart, product]
+        }
+        else{
+            exists.quantity = exists.quantity + 1;
+            const remaining = cart.filter(pd => pd.id !== product.id);
+            newCart = [...remaining, exists];
+        }
+
         setCart(newCart);
         addToDb(product.id)
     }
