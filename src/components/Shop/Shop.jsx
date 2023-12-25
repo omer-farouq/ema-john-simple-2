7d11afad84ea7dc0305b484ac47d7942f3cb8b66
omer-farouq <omerfarouqstd@gmail.com>
2023-12-25 18:08:16 +0600

    data collect from local storage and saved and show

--- src/components/Shop/Shop.jsx
+++ src/components/Shop/Shop.jsx
@@ -14,10 +14,24 @@ const Shop = () => {
         .then(data => setProducsts(data))
     },[])
 
-    useEffect( ()=> {
+    useEffect( () =>{
         const storedCart = getShoppingCart();
-        console.log(storedCart);
-    }, [])
+        const savedCart = [];
+        // step 1: get id of the addedProduct
+        for(const id in storedCart){
+            // step 2: get product from products state by using id
+            const addedProduct = products.find(product => product.id === id)
+            if(addedProduct){
+                // step 3: add quantity
+                const quantity = storedCart[id];
+                addedProduct.quantity = quantity;
+                // step 4: add the added product to the saved cart
+                savedCart.push(addedProduct);
+            }
+            console.log('added Product',addedProduct)
+        }
+        setCart(savedCart);
+    }, [products])
 
     const handleAddtoCart = (product) =>{
         const newCart = [...cart, product];
